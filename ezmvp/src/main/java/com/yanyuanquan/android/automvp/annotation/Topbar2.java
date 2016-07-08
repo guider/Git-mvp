5214f375ebf4dcd8f01af90c1883d5a986b1d9da
guider <guider@yeah.net>
2016-07-08 23:58:26 +0800

    no message

---
+++ ezmvp/src/main/java/com/yanyuanquan/android/automvp/annotation/Topbar2.java
@@ -0,0 +1,9 @@
+package com.yanyuanquan.android.automvp.annotation;
+
+/**
+ * Created by guider on 16/7/8.
+ * Email guider@yeah.net
+ * github https://github.com/guider
+ */
+public class Topbar2 {
+}
