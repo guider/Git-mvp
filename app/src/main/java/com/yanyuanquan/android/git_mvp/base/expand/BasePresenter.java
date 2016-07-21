271ce198089604eed7037f796a8fda78bd8e8261
guider <guider@yeah.net>
2016-07-21 22:02:18 +0800

    no message

--- app/src/main/java/com/yanyuanquan/android/git_mvp/base/expand/BasePresenter.java
+++ app/src/main/java/com/yanyuanquan/android/git_mvp/base/expand/BasePresenter.java
@@ -36,7 +36,7 @@ public class BasePresenter<V, M extends EzModel,D> extends EzPresenter<V, M> {
      *
      * @return
      */
-    public BehaviorSubject<D> getDataSubject() {
+    public BehaviorSubject<D> getCacheDataSubject() {
         return mData;
     }
 
@@ -45,7 +45,7 @@ public class BasePresenter<V, M extends EzModel,D> extends EzPresenter<V, M> {
      *
      * @return
      */
-    public D getData() {
+    public D getCacheData() {
         return mData.getValue();
     }
 
@@ -54,7 +54,7 @@ public class BasePresenter<V, M extends EzModel,D> extends EzPresenter<V, M> {
      *
      * @param data
      */
-    public void setData(D data) {
+    public void setCahceData(D data) {
         mData.onNext(data);
     }
 }
